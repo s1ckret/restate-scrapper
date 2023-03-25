8e877f44c134dfffe5ed3771707ad2d14af29c2e
s1ckret <ivan.kovalenko@s1ckret.com>
2023-03-25 14:44:46 +0200

    feat(parser): add fetching by dates
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,9 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const TELEGRAM_CHANNEL_URL = process.env.TELEGRAM_CHANNEL_URL as string;
 export const TELEGRAM_API_ID = parseInt(process.env.TELEGRAM_API_ID as string);
 export const TELEGRAM_API_HASH = process.env.TELEGRAM_API_HASH as string;
+
+export const FROM_DATE = process.env.FROM_DATE as string;
+export const TO_DATE = process.env.TO_DATE as string;
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { TelegramClient } from "telegram";
 import {
   TELEGRAM_API_ID,
   TELEGRAM_API_HASH,
-  TELEGRAM_CHANNEL_URL
+  TELEGRAM_CHANNEL_URL,
+  FROM_DATE,
+  TO_DATE
 } from "./config";
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
+import { Interface } from "node:readline";
 
 async function main() {
   const rl = readline.createInterface({ input, output });
@@ -28,11 +31,43 @@ async function main() {
 
   const channel = await client.getEntity(TELEGRAM_CHANNEL_URL);
 
-  for await (const message of client.iterMessages(channel, { limit: 2 })) {
-    console.log(message.id, message.text);
+  // Save origin to a database
+
+  const fromDate = toUnixTimestamp(new Date(FROM_DATE));
+  const toDate = toUnixTimestamp(new Date(TO_DATE));
+
+  for await (const message of client.iterMessages(channel, {
+    offsetDate: fromDate,
+    reverse: true
+  })) {
+    if (message.date > toDate) {
+      console.log(
+        `Message '${message.id} is older '${message.date}' than set limit - '${toDate}'`
+      );
+      shutdown(client, rl);
+      return 0;
+    }
+    // Extract message features
+    // Geolocate the building
+    // Open telegraph link with something like bs4
+    // Fetch description
+    // Download all photos
+    // Upload all photos to S3 (save those urls)
+    // Save building to a database
+    // Save ad to a database
+    // Save photos to a database
   }
+  shutdown(client, rl);
+}
+
+function shutdown(client: TelegramClient, rl: Interface) {
+  console.log("Shutting down...");
   client.disconnect();
   rl.close();
+}
+
+function toUnixTimestamp(date: Date): number {
+  return Math.floor(date.getTime() / 1000);
 }
 
 main().catch((err) => {
