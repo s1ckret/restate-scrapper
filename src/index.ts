4827296e26158b3171ca79e0844a595f03671a5c
s1ckret <ivan.kovalenko@s1ckret.com>
2023-03-26 18:00:56 +0300

    feat: save ad to a database
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,30 @@ import {
 } from "./config";
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
-import { Interface } from "node:readline";
-import { extractMessageMetadata } from "./messageMetadataExtracter";
-import { parseMessageBody } from "./messageBodyParser";
+import {
+  extractMessageMetadata,
+  MessageMetadata
+} from "./messageMetadataExtracter";
+import { ParsedMessageBody, parseMessageBody } from "./messageBodyParser";
 import { scrapeWebpage } from "./messageWebPageScrapper";
-import { geocode } from "./geocoder";
+import { geocode, GeocodeResult } from "./geocoder";
 import { downloadImages } from "./imageDownloader";
 import { uploadImages } from "./imageUploader";
+import { ads_currency, PrismaClient } from "@prisma/client";
+const rl = readline.createInterface({ input, output });
+
+const client = new TelegramClient(
+  APPLICATION_NAME,
+  TELEGRAM_API_ID,
+  TELEGRAM_API_HASH,
+  {
+    connectionRetries: 5
+  }
+);
+
+const prisma = new PrismaClient();
 
 async function main() {
-  const rl = readline.createInterface({ input, output });
-
-  const client = new TelegramClient(
-    APPLICATION_NAME,
-    TELEGRAM_API_ID,
-    TELEGRAM_API_HASH,
-    {
-      connectionRetries: 5
-    }
-  );
-
   await client.start({
     phoneNumber: () => rl.question("Enter your phone number: "),
     phoneCode: () => rl.question("Enter the code: "),
@@ -39,46 +43,57 @@ async function main() {
 
   const channel = await client.getEntity(TELEGRAM_CHANNEL_URL);
 
-  // Save origin to a database
-
   const fromDate = toUnixTimestamp(new Date(FROM_DATE));
   const toDate = toUnixTimestamp(new Date(TO_DATE));
 
-  for await (const message of client.iterMessages(channel, {
+  const parameters = {
     offsetDate: fromDate,
     reverse: true,
     limit: 1
-  })) {
+  };
+  for await (const message of client.iterMessages(channel, parameters)) {
     if (message.date > toDate) {
-      console.log(
-        `Message '${message.id} is older '${message.date}' than set limit - '${toDate}'`
-      );
-      shutdown(client, rl);
+      console.log(`Message is older '${message.date}' than - '${toDate}'`);
       return 0;
     }
     try {
       const metadata = extractMessageMetadata(message);
-      const messageBodyFeatures = parseMessageBody(message.text);
+      const features = parseMessageBody(message.text);
       const [title, imageUrls] = await scrapeWebpage(metadata.webpageUrl);
       const geocodingResult = await geocode(
-        messageBodyFeatures.street,
-        messageBodyFeatures.houseNumber
+        features.street,
+        features.houseNumber
       );
-      // Check if ad is not in a database
-      // If true proceed
-      // Save building to a database
-      // Save ad to a database
-      // Save photos to a database
+
+      const adExistInDatabase = !!(await prisma.ad.findFirst({
+        where: { theirId: features.sourceId }
+      }));
+
+      if (adExistInDatabase) {
+        console.log(
+          `Skipped ad with their ID: ${features.sourceId} (already exist)`
+        );
+        continue;
+      }
+
       const images = await downloadImages(imageUrls);
-      const imageKeyPrefix = `${APPLICATION_NAME}/${messageBodyFeatures.sourceId}`;
-      const s3ImageUrls = await uploadImages(
+      const imageKeyPrefix = `${APPLICATION_NAME}/${features.sourceId}`;
+      const [imageKeys, _] = await uploadImages(
         images,
         imageKeyPrefix,
         AWS_S3_BUCKET_NAME
       );
-      console.log(messageBodyFeatures);
-      console.log(s3ImageUrls);
-      // Else skip
+
+      await saveAdToDb(
+        title,
+        message.text,
+        metadata,
+        features,
+        geocodingResult,
+        imageKeys
+      );
+
+      console.log(`Saved ad with their ID: ${features.sourceId}`);
       // wait between messages
       // wait between days
     } catch (e) {
@@ -87,19 +102,104 @@ async function main() {
       }
     }
   }
-  shutdown(client, rl);
 }
 
-function shutdown(client: TelegramClient, rl: Interface) {
+async function saveAdToDb(
+  title: string,
+  description: string,
+  metadata: MessageMetadata,
+  features: ParsedMessageBody,
+  geocodingResult: GeocodeResult,
+  imageKeys: string[]
+) {
+  let currency = "USD" as ads_currency;
+  if (features.currency === "грн") {
+    currency = "UAH" as ads_currency;
+  } else if (features.currency === "€") {
+    currency = "EUR" as ads_currency;
+  }
+
+  const photos = imageKeys.map((it) => {
+    return { key: it };
+  });
+
+  return await prisma.ad.upsert({
+    where: {
+      theirId: features.sourceId
+    },
+    update: {},
+    include: {
+      building: true,
+      origin: true
+    },
+    create: {
+      postedAtSec: metadata.postedAtSec,
+      foundAtSec: Math.floor(new Date().getTime() / 1000),
+      url: metadata.webpageUrl,
+      title: title,
+      description: description,
+      theirId: features.sourceId,
+      roomQty: features.price,
+      totalArea: features.totalArea,
+      atFloor: features.atFloor,
+      price: features.price,
+      currency: currency,
+      readyForLiving: features.readyForLiving,
+      building: {
+        connectOrCreate: {
+          create: {
+            osmId: geocodingResult.osmId,
+            country: features.country,
+            city: features.city,
+            street: geocodingResult.street,
+            houseNumber: features.houseNumber,
+            lat: geocodingResult.lat,
+            lon: geocodingResult.lon,
+            geojson: geocodingResult.geojson,
+            maxFloors: features.buildingMaxFloor
+          },
+          where: {
+            osmId: geocodingResult.osmId
+          }
+        }
+      },
+      origin: {
+        connectOrCreate: {
+          create: {
+            name: "X-Estate Telegram scrapper",
+            url: TELEGRAM_CHANNEL_URL
+          },
+          where: {
+            url: TELEGRAM_CHANNEL_URL
+          }
+        }
+      },
+      photo: {
+        createMany: {
+          data: photos
+        }
+      }
+    }
+  });
+}
+
+async function shutdown() {
   console.log("Shutting down...");
-  client.disconnect();
   rl.close();
+  client.disconnect();
+  await prisma.$disconnect();
 }
 
 function toUnixTimestamp(date: Date): number {
   return Math.floor(date.getTime() / 1000);
 }
 
-main().catch((err) => {
-  console.error(err);
-});
+main()
+  .then(async () => {
+    await shutdown();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await shutdown();
+    process.exit(1);
+  });
